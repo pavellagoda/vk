77265eed20f45de1c8f5a3242e3a009fbd7a9a3f
Павел <@intel.office.webdevs.int>
2012-07-11 19:09:57 +0300

    vk api

--- js/global.js
+++ js/global.js
@@ -2,20 +2,46 @@ $(document).ready(function() {
     $('a#send-post').click(function(){
         var text = $('textarea[name="message"]').val();
         postMessage(text)
+        return false;
+    }) 
+    $('a#send-photo').click(function(){
+        var file = $('input[type="file"]').val().replace(/.+[\\\/]/, "");
+        createAlbum();
     }) 
     function postMessage(text) {
         VK.init({
-            apiId: 3031762
+            apiId: 3031984
         });
         VK.api('wall.post',{
             message: text
         }, function(data) {
-            if (data.response) { // если получен ответ
-                alert('Сообщение отправлено! ID сообщения: ' + data.response.post_id);
+            if (data.response) {
+                alert('Message is sent. message id: ' + data.response.post_id);
                 window.location.href = '/photo.php';
-            } else { // ошибка при отправке сообщения
-                alert('Ошибка! ' + data.error.error_code + ' ' + data.error.error_msg);
+            } else {
+                alert('Error: ' + data.error.error_code + ' ' + data.error.error_msg);
             }
         });
+        VK.UI.button('login_button');
+    }
+    
+    function createAlbum() {
+        VK.init({
+            apiId: 3031984
+        });
+        
+        VK.Api.call('photos.getUploadServer', {'aid':150802604}, function(data){
+            console.log(data)
+        })
+        
+//        VK.api('photos.createAlbum',{
+//            title: 'Море 2012'
+//        }, function(data) {
+//            if (data.response) {
+//                console.log(data.response);
+//            } else {
+//                alert('Error: ' + data.error.error_code + ' ' + data.error.error_msg);
+//            }
+//        });
     }
 })
